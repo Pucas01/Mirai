8048efbe10262680198542594025340b2323c86d
Pucas01 <129163556+Pucas01@users.noreply.github.com>
2026-08-10 16:03:19 +0200

    Some kitty stuff

---
+++ fish/config.fish
@@ -0,0 +1,35 @@
+if status is-interactive
+    set -g fish_greeting
+    set -gx STARSHIP_CONFIG /home/pucas02/GitHub/pucas-dots/fish/starship.toml
+    starship init fish | source
+
+    set -g fish_color_normal d0d0d0
+    set -g fish_color_command 39c5bb
+    set -g fish_color_keyword 80e0e0
+    set -g fish_color_quote e0c05a
+    set -g fish_color_redirection d0d0d0
+    set -g fish_color_end 39c5bb
+    set -g fish_color_error ff6b6b
+    set -g fish_color_param d0d0d0
+    set -g fish_color_comment 666666
+    set -g fish_color_selection --background=2a8a8a
+    set -g fish_color_search_match --background=2a2a2a
+    set -g fish_color_operator 80e0e0
+    set -g fish_color_escape 80e0e0
+    set -g fish_color_autosuggestion 555555
+    set -g fish_color_cwd 39c5bb
+    set -g fish_color_cwd_root ff6b6b
+    set -g fish_pager_color_prefix 39c5bb
+    set -g fish_pager_color_completion d0d0d0
+    set -g fish_pager_color_description 666666
+    set -g fish_pager_color_selected_background --background=2a8a8a
+
+    abbr -a ff fastfetch
+
+    if status is-command-substitution
+        # skip
+    else if not set -q FASTFETCH_SHOWN
+        set -gx FASTFETCH_SHOWN 1
+        fastfetch
+    end
+end
