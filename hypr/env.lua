8d5c32e37e90dd684b0309697ba0a9297919fd58
Pucas01 <129163556+Pucas01@users.noreply.github.com>
2026-08-11 11:28:24 +0200

    update env

--- hypr/env.lua
+++ hypr/env.lua
@@ -5,6 +5,7 @@ hl.on("hyprland.start", function ()
     
     hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
     hl.exec_cmd("systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
+    hl.exec_cmd("systemctl --user start hyprpolkitagent")
 end)
 
 hl.env("XCURSOR_THEME", "Adwaita")
